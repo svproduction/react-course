db33a1400213e07e32aa0390df065005cccbd888
Vlad <mrakobes10@mail.ru>
2022-10-24 21:24:31 +0300

    Add base components

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import { Nav } from "./Nav";
+
+function Header() {
+  return (
+    <header className="header">
+      <a href="#">Logo</a>
+      <Nav></Nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,28 @@
+const Nav = () => {
+  return (
+    <ul className="nav">
+      <li className="nav__item">
+        <a href="./" className="nav__link">
+          Link
+        </a>
+      </li>
+      <li className="nav__item">
+        <a href="./" className="nav__link">
+          Link
+        </a>
+      </li>
+      <li className="nav__item">
+        <a href="./" className="nav__link">
+          Link
+        </a>
+      </li>
+      <li className="nav__item">
+        <a href="./" className="nav__link">
+          Link
+        </a>
+      </li>
+    </ul>
+  );
+};
+
+export { Nav };
